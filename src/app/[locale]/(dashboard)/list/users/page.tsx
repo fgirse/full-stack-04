e0194c4c3e4e701d6a36d1b4ba54cfaa9a3e8fed
fgirse <fgirs@bluewin.ch>
2025-06-28 03:39:38 +0200

    commit 60er

--- src/app/[locale]/(dashboard)/list/users/page.tsx
+++ src/app/[locale]/(dashboard)/list/users/page.tsx
@@ -1,11 +1,191 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import Header from "@/components/header";
+import { auth,  } from "@clerk/nextjs/server";
+import { Class, Prisma, Subject, Users } from "@prisma/client";
 
-const UsersPage = () => {
-  return (
-    <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
-      <h1 className='text-2xl font-bold'>Users</h1>
-    </div>
-  )
+import FormContainer from "@/components/FormContainer";
+import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
+import TableSearch from "@/components/TableSearch";
+
+import {prisma} from "@/lib/prisma";
+import { ITEM_PER_PAGE } from "@/lib/settings";
+
+type UserList = Users & { subjects: Subject[] } & { classes: Class[] } & { phone?: string; address?: string };
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  imageUrl: string; // <-- add this if you want to use 'img'
+  address?: string; // <-- add this if you want to use 'address'
+  phone?: string; // <-- add this if you want to use 'phone'
+  subjects: Subject[];
+  classes: Class[];
 }
 
-export default UsersPage
+const userListPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+} ) => {
+  const { sessionClaims } = auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+  const columns = [
+    {
+      header: "Info",
+      accessor: "info",
+    },
+    {
+      header: "User ID",
+      accessor: "userId",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Subjects",
+      accessor: "subjects",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Classes",
+      accessor: "classes",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Phone",
+      accessor: "phone",
+      className: "hidden lg:table-cell",
+    },
+    {
+      header: "Address",
+      accessor: "address",
+      className: "hidden lg:table-cell",
+    },
+    ...(role === "admin"
+      ? [
+          {
+            header: "Actions",
+            accessor: "action",
+          },
+        ]
+      : []),
+  ];
+
+  const renderRow = (item: UserList) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-300 text-sm hover:bg-plPurpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">
+        <Image
+          src={item.imageUrl || "/noAvatar.png"}
+          alt=""
+          width={40}
+          height={40}
+          className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
+        />
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.name}</h3>
+          <p className="text-xs text-gray-500">{item?.email}</p>
+        </div>
+      </td>
+      <td className="hidden md:table-cell">{item.id}</td>
+      <td className="hidden md:table-cell">
+        {item.subjects.map((subject: { name: any; }) => subject.name).join(",")}
+      </td>
+      <td className="hidden md:table-cell">
+        {item.classes.map((classItem: { name: any; }) => classItem.name).join(",")}
+      </td>
+      <td className="hidden md:table-cell">{item.phone}</td>
+      <td className="hidden md:table-cell">{item.address}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/users/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-plSky">
+              <Image src="/view.png" alt="" width={16} height={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-plPurple">
+            //   <Image src="/delete.png" alt="" width={16} height={16} />
+            // </button>
+            <FormContainer table="student" type="delete" id={item.id} />
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+  const { page, ...queryParams } = searchParams;
+
+  const p = page ? parseInt(page) : 1;
+
+  // URL PARAMS CONDITION
+
+  const query: Prisma.UsersWhereInput = {};
+
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "classId":
+            query.lessons = {
+              some: {
+                classId: parseInt(value),
+              },
+            };
+            break;
+          case "search":
+            query.name = { contains: value, mode: "insensitive" };
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  const [data, count] = await prisma.$transaction([
+    prisma.users.findMany({
+      where: query,
+      include: {
+        subjects: true,
+        classes: true,
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1),
+    }),
+    prisma.user.count({ where: query }),
+  ]);
+
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* TOP */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All users</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-plYellow">
+              <Image src="/filter.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-plYellow">
+              <Image src="/sort.png" alt="" width={14} height={14} />
+            </button>
+            {role === "admin" && (
+              <FormContainer table="user" type="create" />
+            )}
+          </div>
+        </div>
+      </div>
+      {/* LIST */}
+      <Table columns={columns} renderRow={renderRow} data={data} />
+      {/* PAGINATION */}
+      <Pagination page={p} count={count} />
+    </div>
+  
+  );
+};
+
+export default UsersListPage;
+
